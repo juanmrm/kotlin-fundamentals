4bca5cf0b362fbde986a5ca03e1cb72ba336879d
juanmrm <elvior2@hotmail.com>
2019-07-07 18:51:32 +0200

    refactor: learning the basic syntax

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Learning Kotlin Fundamentals
 - Less ceremony than Java
 
 ### Better Java
-- Less ceremony
+- Less ceremony, code is generally simpler
 - Exception handling
 - Language features:
     - Support fon immutable types
@@ -18,5 +18,58 @@ Learning Kotlin Fundamentals
     - Expressions
     - Ranges
 - You can just code, no need even for classes
-  
-    
+
+### Inmutability
+- `var` to declare a mutable variable
+- `val` to declare an inmutable variable
+- When we declare variables in a class, the compiler generates the getter / setter properties automatically
+
+### String Templates
+- Using a simple variable use `$variable`
+- Using a compound variable use `${x.variable}`
+
+### If as an expression
+- If statement works as an expression returning it's value from the if / else statement so we can assing it 
+to a variable like
+```
+    val message = if(q.answer == q.correctAnswer)
+        "You were correct"
+    else "Try again?
+```
+
+### Improve handling of null valuesç
+- We cannot assing a null value to a variable if we don't explicitly say that this variable can either be null or contain a variable with ? `var name: String? = null`
+
+### Switch statement doesn't exist, instead `when`
+- Very powerfull
+```
+when (answer) {
+    correctAnswer -> println("You were correct")
+    else -> println("Try again?")
+}
+```   
+
+### Try - catch as an expression
+- Similar to if
+```
+    val number:Int? = try {
+        Integer.parseInt(q.answer)
+    } catch (e:NumberFormatException) {
+        null
+    }
+```
+
+### Looping constructs
+- `While` and `do` works the same as Java
+- `for` loops are different using `Ranges`, a very powerfull feature of Kotlin using `step, until, downTo, withIndex...`
+
+### Support for exceptions
+- In Java you either catch the exception or rethrow it. Kotlin uses essentially Unchecked exceptions so you don't neet to catch them.
+If you want you can
+```
+try {
+    reader.read()
+} catch (e: IOException) {
+
+} finally { ... }
+```
--- src/main.kt
+++ src/main.kt
@@ -1,23 +1,83 @@
-import com.rsk.Person
+import java.util.*
 
-fun main() {
+fun main(args: Array<String>) {
     println("Hello World!")
+    val q = Question()
+    q.answer = "Four"
+   // q.question = 'Another question'
 
-    val person = Person("Kevin")
+    q.display()
 
-    println("name is ${person.name}")
+    println("The answer to the question ${q.question} is ${q.answer}")
 
-    person.name = "Steve"
+    val message = if(q.answer == q.correctAnswer)
+        "You were correct"
+    else "Try again?"
 
-    println("name is ${person.name}")
+    println(message)
 
-    // person = Person() // Val is for inmutable, so we cannot re assign (using var you can)
+    q.printResult()
 
-    person.display()
+    val number:Int? = try {
+        Integer.parseInt(q.answer)
+    } catch (e:NumberFormatException) {
+        null
+    }
 
-    person.displayWithLambda(::printName)
+    println("Number is $number")
+
+    ranges()
 }
 
-fun printName(name: String) {
-    println(name)
+class Question {
+    var answer: String? = null // To assign a null value we have to put ? in the definition of a variable
+    val correctAnswer = "42"
+    val question: String = "What is the answer to life, the universe of everything?"
+
+    fun display() {
+        println("You said $answer")
+    }
+
+    fun printResult() {
+        when (answer) {
+            correctAnswer -> println("You were correct")
+            else -> println("Try again?")
+        }
+    }
+}
+
+fun ranges() {
+    println("Inclusive Range - Counting from 1 to 10 is steps of 2")
+    for(i in 1..10 step 2) {
+        println(i)
+    }
+
+    println("Inclusive Range - Countdown from 10 to 1")
+    for(i in 10 downTo 1) {
+        println(i)
+    }
+
+    println("Open Range - from 1 to 10, but not including 10")
+    for(i in 1 until 10) {
+        println(i)
+    }
+
+    val numbers = listOf(1,2,3,4,5)
+    for (i in numbers) {
+        println(i)
+    }
+
+    val ages = TreeMap<String, Int>()
+    ages["Kevin"] = 55
+    ages["Sam"] = 24
+    ages["Alex"] = 24
+    ages["Harry"] = 26
+
+    for((name, age) in ages) {
+        println("$name is $age")
+    }
+
+    for((index, el) in numbers.withIndex()) {
+        println("$el at $index")
+    }
 }
